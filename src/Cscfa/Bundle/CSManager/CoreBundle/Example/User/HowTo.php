f731fe85c5ff85dbd1c172b1a3b4583ebb362ea2
matthieu88160 <matthieu.vallance@cscfa.fr>
2015-09-09 17:40:40 +0200

    resolving phpcs errors

--- src/Cscfa/Bundle/CSManager/CoreBundle/Example/User/HowTo.php
+++ src/Cscfa/Bundle/CSManager/CoreBundle/Example/User/HowTo.php
@@ -55,61 +55,59 @@ class HowTo extends Controller implements ExampleInterface
      */
     public function howItWork()
     {
-        
         $manager = $this->getManager();
         $provider = $this->getProvider();
-
-        //Create a new User :
+        
+        // Create a new User :
         $userInstance = $manager->getNewInstance();
         
         $usernameResult = $userInstance->setUsername("UserName");
-        if($usernameResult){
-            //The username has been set
-        }else{
-            //the username setting faild. This can be caused by two issues.
+        if ($usernameResult) {
+            // The username has been set
+        } else {
+            // the username setting faild. This can be caused by two issues.
             
-            if($userInstance->getLastError() == $userInstance::INVALID_USERNAME){
-                //the username is an invalid string
-            }else if($userInstance->getLastError() == $userInstance::DUPLICATE_USERNAME){
-                //the username already exist into the database.
+            if ($userInstance->getLastError() == $userInstance::INVALID_USERNAME) {
+                // the username is an invalid string
+            } else if ($userInstance->getLastError() == $userInstance::DUPLICATE_USERNAME) {
+                // the username already exist into the database.
             }
         }
         /*
          * All of the setters can result false in devers case, but in all of them
          * the builder will conserve an error status.
-         * 
+         *
          * After setting all of the parameters, the instance can be persisted bellow the manager
          */
         
         $manager->persist($userInstance);
         
-        //if this user doesn't used, it possible to remove it
+        // if this user doesn't used, it possible to remove it
         $manager->remove($userInstance);
         
-        
-        //it's possible to create a user instance and get the builder after it
+        // it's possible to create a user instance and get the builder after it
         $user = new User();
         $convertedUser = $manager->convertInstance($user);
         
-        //it's possible to get the user instance from the builder
+        // it's possible to get the user instance from the builder
         $convertedUser->getUser();
-        //and the same for the stack update object
+        // and the same for the stack update object
         $convertedUser->getStackUpdate();
         
-        //It's possible to directly get a user builder from the provider
+        // It's possible to directly get a user builder from the provider
         $builder = $provider->findOneByEmail("email@test.ts");
         unset($builder);
-        //and to get all usernames and emails
+        // and to get all usernames and emails
         $emails = $provider->getAllEmail();
         $usernames = $provider->getAllUsernames();
         unset($emails);
         unset($usernames);
         
-        //note the the manager can test severals things
+        // note the the manager can test severals things
         $manager->isEmailValid("invalid email") === false;
         $manager->isUsernameValid("invalid name") === false;
     }
-    
+
     /**
      * Get the manager.
      * 
@@ -119,10 +117,11 @@ class HowTo extends Controller implements ExampleInterface
      * 
      * @return UserManager
      */
-    public function getManager(){
+    public function getManager()
+    {
         return $this->get("core.manager.user_manager");
     }
-    
+
     /**
      * Get the provider.
      * 
@@ -132,7 +131,8 @@ class HowTo extends Controller implements ExampleInterface
      * 
      * @return UserProvider
      */
-    public function getProvider(){
+    public function getProvider()
+    {
         return $this->get("core.provider.user_provider");
     }
 }
