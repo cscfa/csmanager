2ffe141ebc83dd36a8bb87fbbaa4eeacd9efe301
matthieu88160 <matthieu.vallance@cscfa.fr>
2016-03-08 14:50:43 +0100

    #7 create TaskCaseInterface and TaskStatusInterface

---
+++ src/Cscfa/Bundle/CSManager/UseCaseBundle/Interfaces/TaskCaseInterface.php
@@ -0,0 +1,165 @@
+<?php
+/**
+ * This file is a part of CSCFA UseCase project.
+ * 
+ * The UseCase bundle is part of csmanager project. It's a project manager
+ * written in php with Symfony2 framework.
+ * 
+ * PHP version 5.5
+ * 
+ * @category Interface
+ * @package  CscfaCSManagerUseCaseBundle
+ * @author   Matthieu VALLANCE <matthieu.vallance@cscfa.fr>
+ * @license  http://opensource.org/licenses/MIT MIT
+ * @filesource
+ * @link     http://cscfa.fr
+ */
+namespace Cscfa\Bundle\CSManager\UseCaseBundle\Interfaces;
+
+use Cscfa\Bundle\CSManager\ProjectBundle\Entity\ProjectOwner;
+
+/**
+ * TaskCaseInterface class.
+ *
+ * The TaskCaseInterface class define
+ * the the UseCase / Task interface.
+ *
+ * @category Interface
+ * @package  CscfaCSManagerUseCaseBundle
+ * @author   Matthieu VALLANCE <matthieu.vallance@cscfa.fr>
+ * @license  http://opensource.org/licenses/MIT MIT
+ * @link     http://cscfa.fr
+ */
+interface TaskCaseInterface {
+
+    /**
+     * Get created
+     * 
+     * This method return the
+     * created date of the
+     * task.
+     * 
+     * @return \DateTime
+     */
+    public function getCreated();
+    /**
+     * Set created
+     * 
+     * This method allow to set
+     * the created date of the
+     * task.
+     * 
+     * @param \DateTime $created The created date
+     */
+    private function setCreated(\DateTime $created);
+
+    /**
+     * Get updated
+     * 
+     * This method return the
+     * updated date of the
+     * task.
+     * 
+     * @return \DateTime
+     */
+    public function getUpdated();
+    /**
+     * Set updated
+     * 
+     * This method allow to set
+     * the updated date of the
+     * task.
+     * 
+     * @param \DateTime $updated The updated date
+     */
+    protected function setUpdated(\DateTime $updated);
+
+    /**
+     * Get creator
+     *
+     * This method return the
+     * creator user of the
+     * task.
+     *
+     * @return ProjectOwner
+     */
+    public function getCreator();
+    /**
+     * Set creator
+     *
+     * This method allow to set
+     * the creator user of the
+     * task.
+     * 
+     * @param ProjectOwner $user The creator
+     */
+    protected function setCreator(ProjectOwner $user);
+
+    /**
+     * Get status
+     * 
+     * This method return the
+     * status of the task.
+     * 
+     * @return TaskStatusInterface
+     */
+    public function getStatus();
+    /**
+     * Set status
+     * 
+     * This method allow to set
+     * the status of the task.
+     * 
+     * @param TaskStatusInterface $status The status
+     */
+    public function setStatus(TaskStatusInterface $status);
+
+    /**
+     * Set name
+     * 
+     * This method return the
+     * name of the task.
+     * 
+     * @return string
+     */
+    public function getName();
+    /**
+     * Set name
+     * 
+     * This method allow to set
+     * the name of the task.
+     * 
+     * @param string $name The task name
+     */
+    public function setName($name);
+
+    /**
+     * Get description
+     * 
+     * This method return the
+     * description of the task.
+     * 
+     * @return string
+     */
+    public function getDescription();
+    /**
+     * Set description
+     * 
+     * This method allow to set
+     * the description of the task.
+     * 
+     * @param string $description The description
+     */
+    public function setDescription($description);
+    
+    /**
+     * Update
+     * 
+     * This method perform the
+     * dating update of the task.
+     * 
+     * @return void
+     */
+    public function update();
+    
+}
---
+++ src/Cscfa/Bundle/CSManager/UseCaseBundle/Interfaces/TaskStatusInterface.php
@@ -0,0 +1,71 @@
+<?php
+/**
+ * This file is a part of CSCFA UseCase project.
+ * 
+ * The UseCase bundle is part of csmanager project. It's a project manager
+ * written in php with Symfony2 framework.
+ * 
+ * PHP version 5.5
+ * 
+ * @category Interface
+ * @package  CscfaCSManagerUseCaseBundle
+ * @author   Matthieu VALLANCE <matthieu.vallance@cscfa.fr>
+ * @license  http://opensource.org/licenses/MIT MIT
+ * @filesource
+ * @link     http://cscfa.fr
+ */
+namespace Cscfa\Bundle\CSManager\UseCaseBundle\Interfaces;
+
+/**
+ * TaskStatusInterface class.
+ *
+ * The TaskStatusInterface class define
+ * the the UseCase / Task status interface.
+ *
+ * @category Interface
+ * @package  CscfaCSManagerUseCaseBundle
+ * @author   Matthieu VALLANCE <matthieu.vallance@cscfa.fr>
+ * @license  http://opensource.org/licenses/MIT MIT
+ * @link     http://cscfa.fr
+ */
+interface TaskStatusInterface {
+    
+    /**
+     * Get name
+     * 
+     * This method return the
+     * status name.
+     * 
+     * @return string
+     */
+    public function getName();
+    /**
+     * Set name
+     * 
+     * This method allow to set 
+     * the status name.
+     * 
+     * @param string $name
+     */
+    public function setName($name);
+
+    /**
+     * Get description
+     *
+     * This method return the
+     * status description.
+     *
+     * @return string
+     */
+    public function getDescription();
+    /**
+     * Set description
+     * 
+     * This method allow to set 
+     * the status description.
+     * 
+     * @param string $description
+     */
+    public function setDescription($description);
+    
+}
